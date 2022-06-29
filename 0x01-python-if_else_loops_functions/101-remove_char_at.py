293a6c119056c8dbf742bce17c0d9c84cf219616
Salim Kachemela <sakachemela@gmail.com>
2022-06-29 11:40:10 +0300

    Add solution for task 15: Advance Task

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+A Program that creates a copy of the string, removing the character at the
+position n (not the Python way, the “C array index”).
+Author: Salim J. Kachemela <sakachemela@gmail.com>
+Date: Copyright (c) 2022.
+"""
+
+
+def remove_char_at(str, n):
+    """
+    A function that creates a copy of the string, removing the character at the
+    position n (not the Python way, the “C array index”).
+    """
+    return str.replace(str[n], '')
