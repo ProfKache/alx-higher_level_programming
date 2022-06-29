02a236c3861a1d1f183e7d8f0a1627e5c8a840d3
Salim Kachemela <sakachemela@gmail.com>
2022-06-29 11:13:46 +0300

    Add solution for task 14: Advance Task

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+A Program that prints the ASCII alphabet, in reverse order, alternating
+lowercase and uppercase (z in lowercase and Y in uppercase) , not followed by
+a new line.
+Author: Salim J. Kachemela <sakachemela@gmail.com>
+Date: Copyright (c) 2022.
+"""
+for letter in range(122, 96, -1):
+    if letter % 2 != 0:
+        print(chr(letter).upper(), end='')
+    else:
+        print(chr(letter), end='')
