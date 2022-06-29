bb85b47077702907cf694e490e8fe5c8955271be
Salim Kachemela <sakachemela@gmail.com>
2022-06-29 04:55:42 +0300

    Fix output errors

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 for number in range(0, 100):
     if len(str(number)) == 1:
-        print(f"0{number}", end=', ')
+        print("0{}".format(number), end=', ')
     elif number > 9 and number <= 98:
-        print(f"{number}", end=', ')
+        print("{}".format(number), end=', ')
     else:
-        print(f"{number}")
+        print("{}".format(number))
