946cb080cb2ea33fea3c4529dab2dc3f3732f1c7
Salim Kachemela <sakachemela@gmail.com>
2022-06-29 05:41:04 +0300

    Remove validation for the function

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -3,5 +3,4 @@ def add(a, b):
     """
     This function adds two integers and returns the result.
     """
-    if isinstance(a, int) and isinstance(b, int):
-        return (a + b)
+    return (a + b)
