48afd0b2dae1f0a3024367fb5416504e8baba2b6
Salim Kachemela <sakachemela@gmail.com>
2022-06-29 15:35:41 +0300

    Add solution for task 16: Advance Task

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+A program that prints Python bytecode
+Author: Salim J. Kachemela <sakachemela@gmail.com>
+Date: Copyright (c) 2022.
+"""
+
+
+def magic_calculation(a, b, c):
+    """
+    A function that prints Python bytecode
+    """
+    if a < b:
+        return c
+    elif c > b:
+        return (a + b)
+    return (a * b) - c
