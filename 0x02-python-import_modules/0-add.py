c9d8e1f237aa5a7404e709f5b30f0220f0045d44
Salim Kachemela <sakachemela@gmail.com>
2022-06-30 13:43:05 +0300

    Fix output errors

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -7,6 +7,8 @@ Date: Copyright (c) 2022.
 """
 from add_0 import add
 
-a = 1
-b = 2
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+
+if __name__ == '__main__':
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
