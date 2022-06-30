9e5c0defdf1616dec5cea5379c16dad136cffa56
Salim Kachemela <sakachemela@gmail.com>
2022-06-30 13:57:20 +0300

    Fix ouput errors

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-"""
-A program that imports the function def add(a, b): from the file add_0.py and
-prints the result of the addition 1 + 2 = 3
-Author: Salim J. Kachemela <sakachemela@gmail.com>
-Date: Copyright (c) 2022.
-"""
 if __name__ == '__main__':
     from add_0 import add
 
