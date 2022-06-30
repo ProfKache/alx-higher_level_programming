8cf30231a1654f9467ebc632e3c25ade63166290
Salim Kachemela <sakachemela@gmail.com>
2022-06-30 13:35:54 +0300

    Add solution for task 0

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+A program that imports the function def add(a, b): from the file add_0.py and
+prints the result of the addition 1 + 2 = 3
+Author: Salim J. Kachemela <sakachemela@gmail.com>
+Date: Copyright (c) 2022.
+"""
+from add_0 import add
+
+a = 1
+b = 2
+print(f"{a} + {b} = {add(a, b)}")
