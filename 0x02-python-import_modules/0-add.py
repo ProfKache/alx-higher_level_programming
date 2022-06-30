428c933a6cb8b54d5ddde8622927c09a650b2e2f
Salim Kachemela <sakachemela@gmail.com>
2022-06-30 13:52:54 +0300

    Fix output errors

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -5,10 +5,8 @@ prints the result of the addition 1 + 2 = 3
 Author: Salim J. Kachemela <sakachemela@gmail.com>
 Date: Copyright (c) 2022.
 """
-from add_0 import add
-
-
 if __name__ == '__main__':
+    from add_0 import add
     a = 1
     b = 2
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
