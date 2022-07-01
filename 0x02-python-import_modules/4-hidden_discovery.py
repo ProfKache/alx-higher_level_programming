dd3f423c11e535070496bb3a46c2ae7caa31acb4
Salim Kachemela <sakachemela@gmail.com>
2022-07-01 04:47:09 +0300

    Add solution for task 5

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-    import hidden_4
+    from variable_load_5 import a
 
-    for name in dir(hidden_4):
-        if name.startswith('__'):
-            continue
-        print(name)
+    print(a)
