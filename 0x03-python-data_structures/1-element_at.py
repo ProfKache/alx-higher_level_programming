febf3bd149486768bdc519e96947081f064722a9
Salim Kachemela <sakachemela@gmail.com>
2022-07-03 23:02:23 +0300

    Refactor code output to reduce number of lines

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -8,8 +8,6 @@ def element_at(my_list, idx):
         my_list: The list containing integers to be printed
         idx: The index to which the value is to be located.
     """
-    if idx < 0:
-        return None
-    if idx > (len(my_list) - 1):
+    if (len(my_list) - 1) < idx < 0:
         return None
     return my_list[idx]
