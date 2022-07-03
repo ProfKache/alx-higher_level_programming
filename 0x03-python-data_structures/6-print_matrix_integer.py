bde7ae453dd3953a3a24887d4043905ed2433000
Salim Kachemela <sakachemela@gmail.com>
2022-07-04 00:44:52 +0300

    Add solution for task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    """
+    The function that prints a matrix of integers.
+
+    Args:
+        matrix: The matrix to be printed.
+    """
+    for row in matrix:
+        print(*row)
