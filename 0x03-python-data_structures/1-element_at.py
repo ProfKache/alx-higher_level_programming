95ad0e5ddab49019de9c43d211e6b20e09823454
Salim Kachemela <sakachemela@gmail.com>
2022-07-03 22:37:46 +0300

    Add solution for task 1

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    """
+    This function retrieves an element from a list like in C.
+
+    Args:
+        my_list: The list containing integers to be printed
+        idx: The index to which the value is to be located.
+    """
+    if len(my_list) < idx < 0:
+        return None
+    return my_list[idx]
