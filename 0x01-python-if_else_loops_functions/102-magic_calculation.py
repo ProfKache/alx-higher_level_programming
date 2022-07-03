20188ace9d33b29b5738d9b27eaaafaaa4644bf9
Salim Kachemela <sakachemela@gmail.com>
2022-07-03 13:34:38 +0300

    Add solution for task 8: Advance Task

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -6,12 +6,12 @@ Date: Copyright (c) 2022.
 """
 
 
-def magic_calculation(a, b, c):
-    """
-    A function that prints Python bytecode
-    """
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
     if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
         return c
-    elif c > b:
-        return (a + b)
-    return (a * b) - c
+
+    return sub(a, b)
