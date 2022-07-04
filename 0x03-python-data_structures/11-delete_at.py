40c55a307277cb78dbd2ed0da68da6073de19e14
Salim Kachemela <sakachemela@gmail.com>
2022-07-05 01:33:54 +0300

    Fix errors with output

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -7,7 +7,7 @@ def delete_at(my_list=[], idx=0):
         my_list: The list of integers.
         idx: The index of the item to be deleted.
     """
-    if len(my_list) < idx < 0:
+    if (len(my_list) - 1) < idx < 0:
         return my_list
     del my_list[idx]
     return my_list
