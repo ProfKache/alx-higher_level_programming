fcdaf145f155f218e97891db464058f534b31080
Salim Kachemela <sakachemela@gmail.com>
2022-07-04 21:22:29 +0300

    Fix some output errors

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -14,7 +14,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a = tuple_a[0], 0
     # Dealing with tuple_b
     if not len(tuple_b):
-        tuple_a = 0, 0
+        tuple_b = 0, 0
     elif len(tuple_b) == 1:
-        tuple_a = tuple_b[0], 0
+        tuple_b = tuple_b[0], 0
+
     return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
