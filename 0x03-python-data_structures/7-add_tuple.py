a27086612e7a925eaec8f74499af8465a6c81202
Salim Kachemela <sakachemela@gmail.com>
2022-07-04 21:18:25 +0300

    Fix some ouput errors

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -7,18 +7,14 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a: The first tuple
         tuple_b: The second tuple
     """
-    if len(tuple_a) == 2 and len(tuple_b) == 2:
-        return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    elif len(tuple_a) < 2 or len(tuple_b) < 2:
-        if not len(tuple_a):
-            tuple_a = 0, 0
-            return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-        elif not len(tuple_b):
-            tuple_b = 0, 0
-            return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-        elif tuple_a[0] and len(tuple_a) == 1:
-            tuple_a = tuple_a[0], 0
-            return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-        elif tuple_b[0] and len(tuple_b) == 1:
-            tuple_b = tuple_b[0], 0
-            return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    # Dealing with tuple_a
+    if not len(tuple_a):
+        tuple_a = 0, 0
+    elif len(tuple_a) == 1:
+        tuple_a = tuple_a[0], 0
+    # Dealing with tuple_b
+    if not len(tuple_b):
+        tuple_a = 0, 0
+    elif len(tuple_b) == 1:
+        tuple_a = tuple_b[0], 0
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
