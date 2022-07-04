63d3194a6a444b3e7888e632d4be1692e8ada5fe
Salim Kachemela <sakachemela@gmail.com>
2022-07-04 22:15:47 +0300

    Add solution for task 8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    """
+    The function that returns a tuple with the length of a string and its first
+    character.
+
+    Args:
+        sentence: The string to be evaluated against.
+    """
+    return (len(sentence), sentence[0])
