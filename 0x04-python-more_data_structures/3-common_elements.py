35ac64d324d6dbdd392aedd4be60e465d852334a
Salim Kachemela <sakachemela@gmail.com>
2022-07-07 14:29:20 +0300

    Add solution for task 3

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    """
+    A function that returns a set of common elements in two sets.
+
+    Args:
+        set_1: The first set
+        set_2: The second set
+
+    Return:
+        An intersection of both sets
+    """
+    return (set_1 & set_2)
