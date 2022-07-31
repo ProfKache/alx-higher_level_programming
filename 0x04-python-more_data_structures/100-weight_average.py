af45e9014e68193a06ff9bbd0242a13d7a559a05
Salim Kachemela <sakachemela@gmail.com>
2022-08-01 00:09:43 +0300

    Add solution for task 13 - Advance Task

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -10,3 +10,6 @@ def weight_average(my_list=[]):
     Return:
         the weighted average of all integers tuple
     """
+    if len(my_list) == 0:
+        return 0
+    return sum([(x[0] * x[1]) for x in my_list]) / sum([x[1] for x in my_list])
