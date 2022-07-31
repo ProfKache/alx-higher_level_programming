7bd4caecf59123b220a42ca0bbe04cd53a978431
Salim Kachemela <sakachemela@gmail.com>
2022-08-01 00:02:33 +0300

    Add function prototype for task 13 - Advance Task

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    """
+    A function that returns the weighted average of all integers
+    tuple (<score> , <weight>)
+
+    Args:
+        my_list: a list of all integers
+
+    Return:
+        the weighted average of all integers tuple
+    """
