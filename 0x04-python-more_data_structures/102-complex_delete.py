226c14225c081e762731abc4ac04e0807364b9cc
Salim Kachemela <sakachemela@gmail.com>
2022-08-01 10:43:33 +0300

    Add solution for task 15: Advance Task

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    pass
+    for k, v in a_dictionary.items():
+        if v == value:
+            del a_dictionary[k]
+            return complex_delete(a_dictionary, value)
+
+    return a_dictionary
