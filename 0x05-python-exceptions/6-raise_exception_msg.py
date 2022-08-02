efe20d579a58d2a0bab83bfff5070e24e73948fc
Salim Kachemela <sakachemela@gmail.com>
2022-08-02 22:40:51 +0300

    Add solution for task 6: A unction that raises a name exception with a message

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    """
+    A function that raises a name exception with a message.
+
+    Args:
+        No args
+
+    Return:
+        Prints the raised exception
+    """
+    raise NameError(message)
