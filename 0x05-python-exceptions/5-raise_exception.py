9c87e3c0abd9a50b6a1fa906eef22bfa0da04b16
Salim Kachemela <sakachemela@gmail.com>
2022-08-02 22:14:53 +0300

    Add solution for task 5: A unction that raises a type exception

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def raise_exception():
+    """
+    A function that raises a type exception.
+
+    Args:
+        No args
+    Return:
+        Prints the raised exception
+    """
+    raise TypeError
