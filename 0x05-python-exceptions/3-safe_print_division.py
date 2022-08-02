b11c9f30e53e70bc5d68d1c577a31f796f7a2f1d
Salim Kachemela <sakachemela@gmail.com>
2022-08-02 15:34:39 +0300

    Fix errors with the final output

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,6 +2,12 @@
 def safe_print_division(a, b):
     """
     A function divides 2 integers and prints the result.
+
+    Args:
+        a: the first integer
+        b: the second integer
+    Return:
+        The result of division of a and b
     """
     try:
         result = a / b
