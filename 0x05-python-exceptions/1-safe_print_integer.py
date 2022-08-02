dd453dc71ac5a404e26ffd070471f667851576eb
Salim Kachemela <sakachemela@gmail.com>
2022-08-02 11:29:31 +0300

    Fix new line error

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -8,6 +8,8 @@ def safe_print_integer(value):
     """
     try:
         print('{:d}'.format(value))
+        print()
         return True
     except ValueError:
+        print()
         return False
