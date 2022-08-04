52b6933557b3ae4f54d8d64891e9fb80748c9f8e
Salim Kachemela <sakachemela@gmail.com>
2022-08-04 14:37:25 +0300

    Add solution for task 0: An empty class Square that defines a square.

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Program: 0-square.py
+A program that demonstrates the use of Python classes
+Author: Salim J. Kachemela <sakachemela@gmail.com>
+Copyright (c) 2022
+"""
+
+
+class Square:
+    """A class that defines a square"""
+    pass
