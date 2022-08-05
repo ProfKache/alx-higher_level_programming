a2fce1281cecf3dc63308fd28ba57f02da71743d
Salim Kachemela <sakachemela@gmail.com>
2022-08-05 09:49:52 +0300

    Fix some errors with the output

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -18,7 +18,7 @@ class Square:
             size(int): The size of the circle
         """
         if not isinstance(size, int):
-            print('size must be an integer')
+            raise TypeError('size must be an integer')
         elif size < 0:
-            print('size must be >= 0')
+            raise ValueError('size must be >= 0')
         self.__size = size
