7ecde22f6810e2c05c42e2ad11f37f36b47013bd
Salim Kachemela <sakachemela@gmail.com>
2022-08-05 09:44:26 +0300

    Add solution for task 2: A class Square that defines a square by: (based on 1-square.py)

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Program: 2-square.py
+A program that demonstrates the use of Python classes
+Author: Salim J. Kachemela <sakachemela@gmail.com>
+Copyright (c) 2022
+"""
+
+
+class Square:
+    """A class that defines a square with some instance attributes."""
+
+    def __init__(self, size=0) -> None:
+        """
+        A constructor that initializes the size of the circle.
+
+        Args:
+            size(int): The size of the circle
+        """
+        if isinstance(size, int):
+            print('size must be an integer')
+        elif isinstance(size, str):
+            pass
+        elif size < 0:
+            print('size must be >= 0')
+        self.__size = size
