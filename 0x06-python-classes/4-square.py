d871d3e096f37d5f031d69eaa8685043f9e99368
Salim Kachemela <sakachemela@gmail.com>
2022-08-14 15:13:06 +0300

    Refoctor code to remove unnecessary code

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -17,11 +17,7 @@ class Square:
         Args:
             size(int): The size of the circle
         """
-        if not isinstance(size, int):
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
-        self.__size = size
+        self.size = size
 
     @property
     def size(self):
