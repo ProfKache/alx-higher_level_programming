c4e4366dd2812e5b37b11b63826f88ef6efce220
Salim Kachemela <sakachemela@gmail.com>
2022-09-08 20:08:29 +0300

    Refine the description for the script

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,5 @@
--- A script that lists all rows of the table first_table from the
--- the database hbtn_0c_0
+-- A script that lists all records of the table second_table of the 
+-- database hbtn_0c_0
 SELECT score, name
 FROM second_table
 ORDER BY score DESC;
